246fe8a8a625ab1b4b251da515a3d802f29f8ed3
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2023-04-17 23:42:48 -0400

    seperate button for joke question and answer

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const displayJoke = document.querySelector('.displayJoke');
-const randomJokeButton = document.querySelector('.randomJokeButton');
-
 class JokeGenerator {
     constructor(url) {
         this.url = url;
@@ -10,16 +7,39 @@ async getJoke(category) {
         try {
             const response = await fetch('https://official-joke-api.appspot.com/jokes/random');
             const data = await response.json();
-            return `Question: ${data.setup} Answer:${data.punchline}`;
-
+            console.log(data);
+            return data;
+            // const result = `Question: ${data.setup}\nAnswer: ${data.punchline} `
+            // return result;
         } catch (error) {
             console.error(error);
         }
     }
 }
+
+
 const gettingJoke = new JokeGenerator();
+const randomJokeButton = document.querySelector('.randomJokeButton');
+const endJokeButton = document.querySelector('.endJokeButton');
+const displayJoke = document.querySelector('.displayJoke');
+const startJoke = document.querySelector('.startJoke');
+const endJoke = document.querySelector('.endJoke');
+
+endJokeButton.style.display = "none";
+
+let currentJoke;
+
 
 randomJokeButton.addEventListener('click', async function() {
     const joke = await gettingJoke.getJoke();
-    displayJoke.textContent = joke;
+    startJoke.textContent = joke.setup;
+    currentJoke = joke.setup;
+    endJoke.textContent = '';
+    endJokeButton.style.display = 'block';
+    // displayJoke.innerHTML = joke.replace('\n', '<br>');
+    endJokeButton.addEventListener('click', async function() {
+        if (currentJoke) {
+            endJoke.textContent = joke.punchline;
+        }
+    });
 })
